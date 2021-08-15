1fca6133251844137608822bc37420635c5de071
Krishna <krishnawaghmode1995@gmail.com>
2021-08-15 22:31:39 +0530

    Add todo controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', [UserController::class,'index']);
-Route::get('/users/{user}', [UserController::class,'show']);
+// Users
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::post('/users/{user}/todos', [UserController::class, 'storeTodo']);
+
+// Todos
+Route::put('/todos/{todo}/done', [TodoController::class, 'markAsDone']);
+Route::put('/todos/{todo}/undone', [TodoController::class, 'markAsUndone']);
+Route::put('/todos/{todo}', [TodoController::class, 'update']);
+Route::delete('/todos/{todo}', [TodoController::class, 'delete']);
