09f9f85f794636a85ebc96b2d25455a7c41cc879
Krishna <krishnawaghmode1995@gmail.com>
2021-08-15 16:49:56 +0530

    Add users endpoint. Add controller and resources.Add factory,seed and migration

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+
+              'id'    =>(integer)$this->id,
+              'name'  =>(string)$this->name,
+              'email' =>(integer)$this->email,
+              'todos' =>TodoResource::collection($this->whenLoaded('todos')),
+
+        ];
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function todos()
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
